3c59ad0749593ae957b58b3163815fe4aa17eb3c
wutao <2601285428@qq.com>
2018-11-05 00:21:55 +0800

    待办界面调整

--- coursemgr/view/src/views/course/gTaskInfo.vue
+++ coursemgr/view/src/views/course/gTaskInfo.vue
@@ -5,8 +5,12 @@
     </div>
     <div class="grouped-item" v-for="gTask in gTasks">
       <!--1. 审阅XXX关于“xxxxxx”任务主观题-->
-      审阅<span> {{gTask.targetSerialName}} </span>关于“<span> {{gTask.taskName}} </span>”任务主观题
-      <el-button type="primary" @click="approvalTask(gTask.taskId, gTask.targetSerialNo)">审阅</el-button>
+      <div style="margin-left: 40px">
+        审阅
+        <span class="span-hight-light"> {{gTask.targetSerialName}} </span>
+        关于<span class="span-hight-light"> {{gTask.taskName}} </span>任务的主观题
+      </div>
+      <el-button style="margin-right: 20px" type="primary" @click="approvalTask(gTask.taskId, gTask.targetSerialNo)">审阅</el-button>
     </div>
     <el-dialog :visible.sync="showTaskInfoDialog" width="1240px" title="任务评审">
       <task-detail-comp v-if="showTaskInfoDialog" ref="taskInfoComp"
@@ -23,73 +27,81 @@
 
   import TaskDetailComp from '../../components/taskDetailComp'
 
-    export default {
-      name: "gTaskInfo",
-      components:{
-        TaskDetailComp
+  export default {
+    name: "gTaskInfo",
+    components:{
+      TaskDetailComp
+    },
+    data(){
+      return{
+        operateType:'',
+        selectTaskId:'',
+        markUid:'',
+        showTaskInfoDialog:false,
+        gTasks:[],
+      }
+    },
+    created(){
+      this.getData();
+    },
+    methods:{
+      getData(){
+        //获取我的任务待办列表
+        getMySchedule({courseId:this.variables.courseId, studentNo:this.$store.state.user.token})
+          .then(resp=>{
+            if(resp.status === 0){
+              this.$message.warn('获取我的待办任务失败！');
+              return;
+            }
+            this.gTasks = resp.data;
+          });
       },
-      data(){
-          return{
-            operateType:'',
-            selectTaskId:'',
-            markUid:'',
-            showTaskInfoDialog:false,
-            gTasks:[],
-          }
-      },
-      created(){
-          this.getData();
-      },
-      methods:{
-        getData(){
-          //获取我的任务待办列表
-          getMySchedule({courseId:this.variables.courseId, studentNo:this.$store.state.user.token})
-            .then(resp=>{
-              if(resp.status === 0){
-                this.$message.warn('获取我的待办任务失败！');
-                return;
-              }
-              this.gTasks = resp.data;
-            });
-        },
-        approvalTask(tId, markUid){
+      approvalTask(tId, markUid){
 
-          // 判断当前学生该任务是否已经完成
-          checkStuTaskFinished({ studentNo: this.$store.state.user.token, taskId:tId }).then(res => {
-            if (res.status === 0) {
-              this.$msg.warn(res.msg)
-              return
-            }
-            if (res.data === true) {
-              //显示task信息，填写得分
-              this.selectTaskId = tId;
-              this.operateType = TASK_OPERATOR_TYPE.MARK_POINT;
-              this.markUid = markUid;
-              this.showTaskInfoDialog = true;
-            } else {
-              this.$msg.warning('当前任务未完成，不能对该学生的任务进行审阅操作')
-            }
-          }).catch (err => {
-            this.$msg.error(err)
-          })
-        },
-        closeDlg(){
-          this.showTaskInfoDialog = false;
-          this.getData();
-        }
+        // 判断当前学生该任务是否已经完成
+        checkStuTaskFinished({ studentNo: this.$store.state.user.token, taskId:tId }).then(res => {
+          if (res.status === 0) {
+            this.$msg.warn(res.msg)
+            return
+          }
+          if (res.data === true) {
+            //显示task信息，填写得分
+            this.selectTaskId = tId;
+            this.operateType = TASK_OPERATOR_TYPE.MARK_POINT;
+            this.markUid = markUid;
+            this.showTaskInfoDialog = true;
+          } else {
+            this.$msg.warning('当前任务未完成，不能对该学生的任务进行审阅操作')
+          }
+        }).catch (err => {
+          this.$msg.error(err)
+        })
+      },
+      closeDlg(){
+        this.showTaskInfoDialog = false;
+        this.getData();
       }
     }
+  }
 </script>
 
 <style scoped>
+  .span-hight-light{
+    background-color: #FE9226;
+    color: white;
+    padding: 5px 20px;
+    border-radius: 10px;
+    margin: 0 10px;
+    font-weight: bold;
+  }
   .gtask-panel .grouped-item{
-    padding: 10px;
-    line-height: 30px;
-    width: 90%;
-    margin: 30px auto;
-    background-color: rgba(84, 92, 100, 0.8);
-    border-radius: 4px;
-    color: #fafff8;
+    height: 101px;
+    border: 6px solid #E8EBF0;
+    margin: 17px auto;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
   .gtask-panel .grouped-item > span{
     font-weight: bold;
@@ -97,9 +109,13 @@
     color: #e6a23c;
   }
   .gtask-panel .grouped-item:hover{
-    box-shadow: 0px 0px 10px 4px #3a8ee6;
+    /*box-shadow: 0px 0px 10px 4px #3a8ee6;*/
+    border-color: #9AEDE5;
   }
   .gtask-panel .el-button--primary{
     float: right;
+  }
+  .container-scrollbar{
+    background-color: #F2F2F2;
   }
 </style>
